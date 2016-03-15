1bf3a905197e142dcc534a0f48aa230083884bb5
499535289 <liwei@LiWeideMacBook-Pro.local>
2016-03-15 09:23:54 +0800

    第一次提交到github，初始化项目。

---
+++ RLDailyViews/RLDailyViews/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RLDailyViews
+//
+//  Created by LiWei on 16/3/15.
+//  Copyright (c) 2016年 LiWei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
