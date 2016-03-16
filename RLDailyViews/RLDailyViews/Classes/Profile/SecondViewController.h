831a1d844243da3cd7ebb17c1293deb3338eea4b
499535289 <liwei@liweidemacbook-pro.local>
2016-03-16 22:57:50 +0800

    增加文件

---
+++ RLDailyViews/RLDailyViews/Classes/Profile/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  RLDailyViews
+//
+//  Created by LiWei on 16/3/16.
+//  Copyright © 2016年 LiWei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
